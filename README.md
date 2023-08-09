8d2af4c759086ef71cecbad1338ed34ccdcd7996
ibarakoi <ddawng184@gmail.com>
2023-08-10 01:52:13 +0700

    finished

---
+++ README.md
@@ -0,0 +1,19 @@
+# PRJ301 Exercises
+
+- [Exercise 01 - Servlet](doc/ex01.md)
+- [Exercise 02 - JDBC](doc/ex02.md)
+- [Exercise 03](doc/ex03.md)
+- [Exercise 04](doc/ex04.md)
+- [Exercise 05](doc/ex05.md)
+- [Exercise 06](doc/ex06.md)
+- [Exercise 07](doc/ex07.md)
+- [Exercise 08](doc/ex08.md)
+
+- [Issues and Solutions](doc/support-issues.md)
+
+## Instruction
+
+Clone demo project to your computer
+```
+git clone https://github.com/dunghuynh-teaching/prj301-demo.git .
+```
---
+++ prj301-demo3/src/main/java/com/fptuni/prj301/demo/Users/UserDTO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fptuni.prj301.demo.Users;
+
+import java.util.Date;
+
+/**
+ *
+ * @author DUNGHUYNH
+ */
+public class UserDTO {
+    
+    private String username;
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
+    private Date loginDate;
+
+    /**
+     * Get the value of username
+     *
+     * @return the value of username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @param username new value of username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
